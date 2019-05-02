142673d4cf2ed26f5ec6fce47c74e66ddb5513f1
Wojciech Marek <marek.wojciech@outlook.com>
2019-05-02 21:55:31 +0200

    Add standard .net core identity

---
+++ LocalMarketplace/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LocalMarketplace.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LocalMarketplace.Areas.Identity.IdentityHostingStartup))]
+namespace LocalMarketplace.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<LocalMarketplaceContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("LocalMarketplaceIdentityConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<LocalMarketplaceContext>();
+            });
+        }
+    }
+}
